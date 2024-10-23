b0dba0bb7862984dd475c20aef01a476a746bc77
Kigogo254 <kigogoian@gmail.com>
2024-10-23 13:40:46 +0300

    Trial

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 require('dotenv').config();
-
-
-
 const app = express();
 
 
